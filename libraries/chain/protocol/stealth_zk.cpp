b0030a3b548d1931344572ea6cbbdc27e30879e9
Gordon Shumway <gshumway@openmailbox.org>
2016-12-01 08:01:06 -0800

    note encryption

--- libraries/chain/protocol/stealth_zk.cpp
+++ libraries/chain/protocol/stealth_zk.cpp
@@ -24,6 +24,8 @@
 #include <graphene/chain/protocol/stealth_zk.hpp>
 #include <graphene/chain/database.hpp>
 #include <fc/crypto/rand.hpp>
+#include <fc/crypto/dh.hpp>
+#include <fc/crypto/blowfish.hpp>
 #include <array>
 
 namespace graphene { namespace chain {
@@ -166,10 +168,6 @@ fc::uint256 stealth_note::nullifier(const stealth_spending_key &a_sk) const
     return PRF_nf(a_sk.value, nullifier_base);
 }
 
-
-
-
-
 stealth_note_plaintext::stealth_note_plaintext()
 {
 }
@@ -207,15 +205,110 @@ binary stealth_note_plaintext::encrypt(stealth_note_encryption &encryptor,
     return encryptor.encrypt(transmission_key, fc::raw::pack(*this));
 }
 
+//////////////////////////////////////////////////////////////////
+// Note encryption/decryption
+//////////////////////////////////////////////////////////////////
+
+#define NOTEENCRYPTION_CIPHER_KEYSIZE 32
 
 
+fc::uint256 KDF(
+    const fc::uint256 &dhsecret,
+    const fc::uint256 &epk,
+    const fc::uint256 &pk_enc,
+    const fc::uint256 &hSig,
+    unsigned char nonce
+   )
+{
+    if (nonce == 0xff) {
+        throw std::logic_error("no additional nonce space for KDF");
+    }
+
+    boost::array<char, 129> block;
+    memcpy(&block[0], hSig.data(), hSig.data_size());
+    memcpy(&block[32], dhsecret.data(), dhsecret.data_size());
+    memcpy(&block[64], epk.data(), epk.data_size());
+    memcpy(&block[96], pk_enc.data(), pk_enc.data_size());
+    block[128] = nonce;
+
+    return fc::sha256::hash(block.data(), block.size());
+}
+
+stealth_note_encryption::stealth_note_encryption(fc::uint256 sig) :
+    nonce(0),
+    h_sig(sig)
+{
+    // Create the ephemeral keypair
+    ephemeral_secret_key = random_uint256();
+    ephemeral_public_key =
+            stealth_note_encryption::generate_public_key(
+                ephemeral_secret_key
+                );
+}
+
+binary stealth_note_encryption::encrypt(const fc::uint256 &encryption_public_key,
+                                        const binary &plaintext)
+{
+
+    fc::diffie_hellman dh;
+    dh.priv_key.assign(ephemeral_secret_key.data(),
+                       ephemeral_secret_key.data() +
+                       ephemeral_secret_key.data_size());
+    dh.generate_pub_key();
+    dh.compute_shared_key(encryption_public_key.data(),
+                          encryption_public_key.data_size());
+    fc::uint256 dhsecret(&dh.shared_key[0],
+            dh.shared_key.size()); // TODO: check if it is correct
+
+    // Construct the symmetric key
+    fc::uint256 K = KDF(dhsecret, ephemeral_public_key, encryption_public_key,
+        h_sig, nonce);
+
+    // Increment the number of encryptions we've performed
+    nonce++;
+
+    binary ciphertext(plaintext);
+
+    fc::blowfish bf;
+    bf.start(reinterpret_cast<unsigned char*>(K.data()), K.data_size());
+    bf.encrypt(reinterpret_cast<unsigned char*>(ciphertext.data()), ciphertext.size());
+
+    return ciphertext;
+}
+
+fc::uint256 stealth_note_encryption::generate_secret_key(const stealth_spending_key &paying_key)
+{
+
+}
+
+fc::uint256 stealth_note_encryption::generate_public_key(const fc::uint256 &secret_key)
+{
+
+}
+
+
+stealth_note_decryption::stealth_note_decryption(fc::uint256 s_k):
+    secret_key(s_k)
+{
+    public_key = stealth_note_encryption::generate_public_key(secret_key);
+}
+
+binary stealth_note_decryption::decrypt(const binary &ciphertext, const fc::uint256 &ephemeral_public_key, const fc::uint256 &h_sig, unsigned char nonce) const
+{
+
+}
 
 fc::uint256 stealth_input::nullifier() const
 {
     return note.nullifier(spending_key);
 }
 
+stealth_note stealth_output::note(const fc::uint256 &nullifier_base,
+                                  const fc::uint256 &trapdoor, size_t i,
+                                  const fc::uint256 &h_sig) const
+{
 
+}
 
 }}
 
